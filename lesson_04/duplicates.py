b9ddf0d09a6795a8c2d7ab70d43bc3760bf348ec
Ignasiya <ignasiya93@gmail.com>
2023-11-10 21:40:18 +0300

    Урок 4

--- lesson_04/duplicates.py
+++ lesson_04/duplicates.py
@@ -1,5 +1,9 @@
+from collections import Counter
+
+
 def duplicates(data):
-    return list(filter(lambda x: data.count(x) > 1, data))
+    counts = Counter(data)
+    return list(filter(lambda x: counts.get(x) > 1, data))
 
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 7, 9]
