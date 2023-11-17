266a9817028db6ad7b202437818b0d01f5c40c26
Ignasiya <ignasiya93@gmail.com>
2023-11-17 21:10:35 +0300

    Урок 6

--- lesson_06/home_work_06/binary_search.py
+++ lesson_06/home_work_06/binary_search.py
@@ -1,3 +1,6 @@
+import math
+
+
 def binary_search(arr: [int], elem: int) -> int:
     start = 0
     end = len(arr) - 1
@@ -17,11 +20,29 @@ def binary_search(arr: [int], elem: int) -> int:
     binary_search_recursive(arr, elem, start, end)
 
 
+def binary_search_alt(arr, target):
+    arr.sort()
+
+    left = 0
+    right = len(arr)
+
+    while left <= right:
+        mid = math.floor((left + right) / 2)
+
+        if target > arr[mid]:
+            left = mid + 1
+        elif target < arr[mid]:
+            right = mid - 1
+        else:
+            return arr.index(target)
+
+
 if __name__ == '__main__':
     arr = [1, 2, 7, 6, 11, 33, 45]
     number = int(input("Введите искомый элемент: "))
     result = binary_search(arr, number)
+    # result = binary_search_alt(arr, number)
     if result == -1:
         print("Искомого элемента нет в массиве")
     else:
-        print(f"Искомый элемент: {number} найден по индексу: {result}")
+        print(f"Искомый элемент {number} найден")
